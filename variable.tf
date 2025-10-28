5bb8c08deab13151c4e8a5c7c2cb7f3b5ff41c39
Jemin Kalakoti <jeminkalakoti@gmail.com>
2025-10-27 19:55:32 -0500

    commit

---
+++ variable.tf
@@ -0,0 +1,77 @@
+variable "vpc_cidr" {
+  description = "Plesae specify the CIDR block for the VPC"
+  type        = string
+}
+
+variable "project_name"{
+    type = string
+
+}
+
+variable "environment"{
+    type = string
+
+}
+
+variable "vpc_tags"{
+    type = map
+    default = {}
+}
+
+variable "igw_tags"{
+    type = map
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+  type        = list
+}
+  
+variable "public_subnet_tags"{
+    type = map
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+  type        = list
+
+}
+
+variable "private_subnet_tags"{
+    type = map
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+  type        = list
+}
+
+variable "database_subnet_tags"{
+    type = map
+    default = {}
+}
+
+variable "public_route_table_tags"{
+    type = map
+    default = {}
+}
+
+variable "private_route_table_tags"{
+    type = map
+    default = {}
+}
+
+variable "database_route_table_tags"{
+    type = map
+    default = {}
+}
+
+variable "eip_tags"{
+    type = map
+    default = {}    
+}
+
+variable "nat_gateway_tags"{
+    type = map
+    default = {}
+}
